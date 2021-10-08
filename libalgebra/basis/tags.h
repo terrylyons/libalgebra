f31c4ae860066cd000f0dd473b5cd262c40da722
Sam Morley <sam@inakleinbottle.com>
2021-10-08 15:48:11 +0100

    Added docs to utility types

--- libalgebra/basis/tags.h
+++ libalgebra/basis/tags.h
@@ -12,6 +12,15 @@
 namespace alg {
 namespace basis {
 
+
+/**
+ * @brief Tag to indicate that a basis has an associated degree
+ *
+ * This tag indicates that the basis has a degree associated with it
+ * and communicates the maximum degree values that can be stored.
+ *
+ * @tparam D Maximum degree, must be non-zero
+ */
 template <DEG D> struct with_degree
 {
     static const typename alg::utils::enable_if<(D > 0), DEG>::type max_degree;
@@ -20,8 +29,21 @@ template <DEG D> struct with_degree
 template <DEG D> const typename alg::utils::enable_if<(D > 0), DEG>::type
         with_degree<D>::max_degree = D;
 
+/**
+ * @brief Tag to indicate that a basis has no associated degree
+ *
+ */
 struct without_degree {};
 
+
+/**
+ * @brief Tag to indicate that the basis as an ordering
+ *
+ * Indicates that a basis is ordered and provides the ordering for both basis keys
+ * and key-value pairs.
+ *
+ * @tparam OrderOperator Order operator for basis keys
+ */
 template <typename OrderOperator> struct ordered
 {
     typedef OrderOperator order;
@@ -37,6 +59,8 @@ template <typename OrderOperator> struct ordered
     };
 };
 
+
+/// Tag to indicate that a basis has no ordering
 struct unordered {};
 
 } // namespace basis

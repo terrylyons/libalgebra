eaf3177006a2c27bb8fb4ea533804f9d071cab4d
Sam Morley <sam@inakleinbottle.com>
2021-10-29 15:28:59 +0100

    Implemented emplace method for dense_storage_base

--- libalgebra/vectors/dense_storage.h
+++ libalgebra/vectors/dense_storage.h
@@ -9,6 +9,7 @@
 #include <initializer_list>
 #include <iostream>
 #include <memory>
+#include <cassert>
 
 #include <boost/serialization/array.hpp>
 
@@ -123,6 +124,15 @@ struct dense_storage_base {
     {
         return m_type == borrowed_mut;
     }
+
+    template <typename... Args>
+    value_type& emplace(size_type idx, Args&&... args) noexcept(noexcept(alloc_traits::construct(m_alloc, m_data + idx, std::forward<Args>(args)...)))
+    {
+        assert(idx < m_size);
+        alloc_traits::construct(m_alloc, m_data + idx, std::forward<Args>(args)...);
+        return m_data[idx];
+    }
+
 };
 
 }// namespace dtl
@@ -317,7 +327,7 @@ public:
     {
         size_type i = 0;
         for (auto v : args) {
-            m_base.m_data[i++] = v;
+            m_base.template emplace(i++, v);
         }
     }
 

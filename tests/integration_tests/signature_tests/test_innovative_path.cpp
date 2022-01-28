11847f3805f4b819bc8199a70b21a71e13acf1fa
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2022-01-28 16:00:17 +0000

    Moved signature tests into integration tests dir (#63)
    * Moved signature tests into integration tests dir
    
    * Added option and guards to disable integration tests
    
    Co-authored-by: Sam Morley <sam@inakleinbottle.com>

--- tests/integration_tests/signature_tests/test_innovative_path.cpp
+++ tests/integration_tests/signature_tests/test_innovative_path.cpp
@@ -3,30 +3,29 @@
 //
 
 #include <algorithm>
-#include <vector>
 #include <map>
+#include <vector>
 
 #include <UnitTest++/UnitTest++.h>
 
-#include <libalgebra/libalgebra.h>
-#include <libalgebra/alg_types.h>
+#include "libalgebra/alg_types.h"
+#include "libalgebra/libalgebra.h"
 
-
-#include "../../common/time_and_details.h"
-#include "../../common/rng.h"
-#include "../../common/helpers.h"
+#include "tests/common/helpers.h"
+#include "tests/common/rng.h"
+#include "tests/common/time_and_details.h"
 
 #include "generic_coefficient.h"
 #include "generic_lie_increment.h"
 #include "generic_path.h"
 
-template <unsigned Width>
+template<unsigned Width>
 generic_path<Width> make_innovative_path(unsigned length)
 {
-    std::vector<generic_lie_increment<Width, int32_t> > tmp;
+    std::vector<generic_lie_increment<Width, int32_t>> tmp;
     tmp.reserve(length);
     mt19937 rng;
-    UNIFORM_INT_DIST<int32_t> dist(0, Width-1);
+    UNIFORM_INT_DIST<int32_t> dist(0, Width - 1);
 
     UNIFORM_INT_DIST<int32_t> distn(-5, 5);
     UNIFORM_INT_DIST<int32_t> distd(1, 60);
@@ -35,32 +34,35 @@ generic_path<Width> make_innovative_path(unsigned length)
     seen.reserve(length);
 
     size_t key = dist(rng);
-    std::vector<generic_coefficient<int32_t> > incr_tmp;
+    std::vector<generic_coefficient<int32_t>> incr_tmp;
     incr_tmp.reserve(Width);
-    for (size_t i=0; i <Width; ++i) {
+    for (size_t i = 0; i < Width; ++i) {
         if (i == key) {
             incr_tmp.push_back(generic_coefficient<int32_t>(distn(rng), distd(rng)));
-        } else {
+        }
+        else {
             incr_tmp.push_back(generic_coefficient<int32_t>(0, 1));
         }
     }
     tmp.push_back(generic_lie_increment<Width, int32_t>(incr_tmp));
     seen.push_back(key);
 
-    for (size_t i=1; i < length; ++i) {
+    for (size_t i = 1; i < length; ++i) {
         while (std::find(seen.begin(), seen.end(), key) != seen.end()) {
             key = dist(rng);
         }
         incr_tmp.clear();
         incr_tmp.reserve(Width);
-        for (size_t i=0; i <Width; ++i) {
+        for (size_t i = 0; i < Width; ++i) {
             if (i == key) {
                 incr_tmp.push_back(generic_coefficient<int32_t>(distn(rng), distd(rng)));
-            } else {
+            }
+            else {
                 incr_tmp.push_back(generic_coefficient<int32_t>(0, 1));
             }
         }
-        tmp.push_back(generic_lie_increment<Width, int32_t>(incr_tmp));;
+        tmp.push_back(generic_lie_increment<Width, int32_t>(incr_tmp));
+        ;
         seen.push_back(key);
     }
     assert(tmp.size() == length);
@@ -71,23 +73,19 @@ generic_path<Width> make_innovative_path(unsigned length)
 namespace {
 
 #if __cplusplus >= 201103UL
-    constexpr
+constexpr
 #endif
-    inline size_t binomial_coeff(size_t n, size_t k)
-    {
-        return (k > n) ? 0 :
-               (k == 0 || k == n) ? 1 :
-               binomial_coeff(n-1, k-1) + binomial_coeff(n-1, k);
-    }
-
+        inline size_t
+        binomial_coeff(size_t n, size_t k)
+{
+    return (k > n) ? 0 : (k == 0 || k == n) ? 1
+                                            : binomial_coeff(n - 1, k - 1) + binomial_coeff(n - 1, k);
 }
 
+}// namespace
 
-
-
-template <unsigned Width, unsigned Depth>
-struct GenericFixture
-{
+template<unsigned Width, unsigned Depth>
+struct GenericFixture {
     static const unsigned width = Width;
     static const unsigned depth = Depth;
     generic_path<width> path;
@@ -101,8 +99,7 @@ struct GenericFixture
     typedef alg::coefficients::double_field double_field;
     typedef alg::coefficients::float_field float_field;
 
-    struct rational_sparse_framework
-    {
+    struct rational_sparse_framework {
         typedef typename rational_field::S S;
         typedef typename rational_field::Q Q;
         typedef alg::free_tensor_basis<width, depth> TBASIS;
@@ -111,23 +108,21 @@ struct GenericFixture
         typedef alg::vectors::sparse_vector<
                 TBASIS,
                 rational_field,
-                std::map<typename TBASIS::KEY, S>
-        > SPTENS;
+                std::map<typename TBASIS::KEY, S>>
+                SPTENS;
 
         typedef alg::vectors::sparse_vector<
                 LBASIS,
                 rational_field,
-                std::map<typename LBASIS::KEY, S>
-        > SPLIE;
+                std::map<typename LBASIS::KEY, S>>
+                SPLIE;
 
         typedef alg::free_tensor<rational_field, width, depth, alg::vectors::sparse_vector> TENSOR;
         typedef alg::lie<rational_field, width, depth, alg::vectors::sparse_vector> LIE;
         typedef alg::maps<rational_field, width, depth, TENSOR, LIE> MAPS;
-
     };
 
-    struct rational_dense_framework
-    {
+    struct rational_dense_framework {
         typedef typename rational_field::S S;
         typedef typename rational_field::Q Q;
         typedef alg::free_tensor_basis<width, depth> TBASIS;
@@ -135,21 +130,20 @@ struct GenericFixture
 
         typedef alg::vectors::dense_vector<
                 TBASIS,
-                rational_field
-        > DTENS;
+                rational_field>
+                DTENS;
 
         typedef alg::vectors::sparse_vector<
                 LBASIS,
-                rational_field
-        > DLIE;
+                rational_field>
+                DLIE;
 
         typedef alg::free_tensor<rational_field, width, depth, alg::vectors::dense_vector> TENSOR;
         typedef alg::lie<rational_field, width, depth, alg::vectors::dense_vector> LIE;
         typedef alg::maps<rational_field, width, depth, TENSOR, LIE> MAPS;
     };
 
-    struct rational_hybrid_framework
-    {
+    struct rational_hybrid_framework {
         typedef typename rational_field::S S;
         typedef typename rational_field::Q Q;
         typedef alg::free_tensor_basis<width, depth> TBASIS;
@@ -159,24 +153,22 @@ struct GenericFixture
                 TBASIS,
                 rational_field,
                 alg::vectors::policy::basic_resize_policy,
-                std::map<typename TBASIS::KEY, S>
-        > HTENS;
+                std::map<typename TBASIS::KEY, S>>
+                HTENS;
 
         typedef alg::vectors::hybrid_vector<
                 LBASIS,
                 rational_field,
                 alg::vectors::policy::basic_resize_policy,
-                std::map<typename LBASIS::KEY, S>
-        > HLIE;
+                std::map<typename LBASIS::KEY, S>>
+                HLIE;
 
         typedef alg::free_tensor<rational_field, width, depth, alg::vectors::hybrid_vector> TENSOR;
         typedef alg::lie<rational_field, width, depth, alg::vectors::hybrid_vector> LIE;
         typedef alg::maps<rational_field, width, depth, TENSOR, LIE> MAPS;
     };
 
-
-    struct double_sparse_framework
-    {
+    struct double_sparse_framework {
         typedef typename double_field::S S;
         typedef typename double_field::Q Q;
         typedef alg::free_tensor_basis<width, depth> TBASIS;
@@ -185,22 +177,21 @@ struct GenericFixture
         typedef alg::vectors::sparse_vector<
                 TBASIS,
                 double_field,
-                std::map<typename TBASIS::KEY, S>
-        > SPTENS;
+                std::map<typename TBASIS::KEY, S>>
+                SPTENS;
 
         typedef alg::vectors::sparse_vector<
                 LBASIS,
                 double_field,
-                std::map<typename LBASIS::KEY, S>
-        > SPLIE;
+                std::map<typename LBASIS::KEY, S>>
+                SPLIE;
 
         typedef alg::free_tensor<double_field, width, depth, alg::vectors::sparse_vector> TENSOR;
         typedef alg::lie<double_field, width, depth, alg::vectors::sparse_vector> LIE;
         typedef alg::maps<double_field, width, depth, TENSOR, LIE> MAPS;
     };
 
-    struct double_dense_framework
-    {
+    struct double_dense_framework {
         typedef typename double_field::S S;
         typedef typename double_field::Q Q;
         typedef alg::free_tensor_basis<width, depth> TBASIS;
@@ -208,21 +199,20 @@ struct GenericFixture
 
         typedef alg::vectors::dense_vector<
                 TBASIS,
-                double_field
-        > DTENS;
+                double_field>
+                DTENS;
 
         typedef alg::vectors::sparse_vector<
                 LBASIS,
-                double_field
-        > DLIE;
+                double_field>
+                DLIE;
 
         typedef alg::free_tensor<double_field, width, depth, alg::vectors::dense_vector> TENSOR;
         typedef alg::lie<double_field, width, depth, alg::vectors::dense_vector> LIE;
         typedef alg::maps<double_field, width, depth, TENSOR, LIE> MAPS;
     };
 
-    struct double_hybrid_framework
-    {
+    struct double_hybrid_framework {
         typedef typename double_field::S S;
         typedef typename double_field::Q Q;
         typedef alg::free_tensor_basis<width, depth> TBASIS;
@@ -232,23 +222,22 @@ struct GenericFixture
                 TBASIS,
                 double_field,
                 alg::vectors::policy::basic_resize_policy,
-                std::map<typename TBASIS::KEY, S>
-        > HTENS;
+                std::map<typename TBASIS::KEY, S>>
+                HTENS;
 
         typedef alg::vectors::hybrid_vector<
                 LBASIS,
                 double_field,
                 alg::vectors::policy::basic_resize_policy,
-                std::map<typename LBASIS::KEY, S>
-        > HLIE;
+                std::map<typename LBASIS::KEY, S>>
+                HLIE;
 
         typedef alg::free_tensor<double_field, width, depth, alg::vectors::hybrid_vector> TENSOR;
         typedef alg::lie<double_field, width, depth, alg::vectors::hybrid_vector> LIE;
         typedef alg::maps<double_field, width, depth, TENSOR, LIE> MAPS;
     };
 
-    struct float_sparse_framework
-    {
+    struct float_sparse_framework {
         typedef typename float_field::S S;
         typedef typename float_field::Q Q;
         typedef alg::free_tensor_basis<width, depth> TBASIS;
@@ -257,22 +246,21 @@ struct GenericFixture
         typedef alg::vectors::sparse_vector<
                 TBASIS,
                 float_field,
-                std::map<typename TBASIS::KEY, S>
-        > SPTENS;
+                std::map<typename TBASIS::KEY, S>>
+                SPTENS;
 
         typedef alg::vectors::sparse_vector<
                 LBASIS,
                 float_field,
-                std::map<typename LBASIS::KEY, S>
-        > SPLIE;
+                std::map<typename LBASIS::KEY, S>>
+                SPLIE;
 
         typedef alg::free_tensor<float_field, width, depth, alg::vectors::sparse_vector> TENSOR;
         typedef alg::lie<float_field, width, depth, alg::vectors::sparse_vector> LIE;
         typedef alg::maps<float_field, width, depth, TENSOR, LIE> MAPS;
     };
 
-    struct float_dense_framework
-    {
+    struct float_dense_framework {
         typedef typename float_field::S S;
         typedef typename float_field::Q Q;
         typedef alg::free_tensor_basis<width, depth> TBASIS;
@@ -280,21 +268,20 @@ struct GenericFixture
 
         typedef alg::vectors::dense_vector<
                 TBASIS,
-                float_field
-        > DTENS;
+                float_field>
+                DTENS;
 
         typedef alg::vectors::sparse_vector<
                 LBASIS,
-                float_field
-        > DLIE;
+                float_field>
+                DLIE;
 
         typedef alg::free_tensor<float_field, width, depth, alg::vectors::dense_vector> TENSOR;
         typedef alg::lie<float_field, width, depth, alg::vectors::dense_vector> LIE;
         typedef alg::maps<float_field, width, depth, TENSOR, LIE> MAPS;
     };
 
-    struct float_hybrid_framework
-    {
+    struct float_hybrid_framework {
         typedef typename float_field::S S;
         typedef typename float_field::Q Q;
         typedef alg::free_tensor_basis<width, depth> TBASIS;
@@ -304,15 +291,15 @@ struct GenericFixture
                 TBASIS,
                 float_field,
                 alg::vectors::policy::basic_resize_policy,
-                std::map<typename TBASIS::KEY, S>
-        > HTENS;
+                std::map<typename TBASIS::KEY, S>>
+                HTENS;
 
         typedef alg::vectors::hybrid_vector<
                 LBASIS,
                 float_field,
                 alg::vectors::policy::basic_resize_policy,
-                std::map<typename LBASIS::KEY, S>
-        > HLIE;
+                std::map<typename LBASIS::KEY, S>>
+                HLIE;
 
         typedef alg::free_tensor<float_field, width, depth, alg::vectors::hybrid_vector> TENSOR;
         typedef alg::lie<float_field, width, depth, alg::vectors::hybrid_vector> LIE;
@@ -324,45 +311,41 @@ struct GenericFixture
                        expected_float_error(1.0e-6f)
     {}
 
-    size_t sig_support(unsigned increments=width)
+    size_t sig_support(unsigned increments = width)
     {
         return binomial_coeff(increments + depth, depth);
     }
 };
 
+SUITE(innovative_path_5_tests)
+{
 
+    typedef GenericFixture<5, 5> Fixture;
 
-
-SUITE(innovative_path_5_tests) {
-
-typedef GenericFixture<5, 5> Fixture;
-
+#include "accuracy_suite.ins"
 #include "double_path_suite.ins"
 #include "float_path_suite.ins"
 #include "rational_path_suite.ins"
-#include "accuracy_suite.ins"
-
 }
 
-
-SUITE(innovative_path_10_tests) {
+SUITE(innovative_path_10_tests)
+{
 
     typedef GenericFixture<10, 3> Fixture;
 
+#include "accuracy_suite.ins"
 #include "double_path_suite.ins"
 #include "float_path_suite.ins"
 #include "rational_path_suite.ins"
-#include "accuracy_suite.ins"
-
 }
 
-SUITE(innovative_path_15_tests) {
+SUITE(innovative_path_15_tests)
+{
 
     typedef GenericFixture<15, 2> Fixture;
 
+#include "accuracy_suite.ins"
 #include "double_path_suite.ins"
 #include "float_path_suite.ins"
 #include "rational_path_suite.ins"
-#include "accuracy_suite.ins"
-
 }

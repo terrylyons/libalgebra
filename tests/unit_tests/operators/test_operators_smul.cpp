d622b1b866acf9935013cb854011eef80bc32098
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2022-01-28 15:21:15 +0000

    Free extension operators (#61) (#62)
    * Free extension operators (#61)
    
    * Moved tags to separate header
    
    * Added new free_extension.h
    
    * Updated free extensions
    
    * Added simple tests for extension operators
    
    Co-authored-by: Sam Morley <sam@inakleinbottle.com>
    
    * Added tensor defined operator
    
    * Added tests for tensor defined operators
    
    * Implemented sum and scalar multiply operators
    
    * Added implementation for composition of operators
    
    * Added tests for composition operators
    
    * Added basic tests for operator sums
    
    * added basic tests for operator scalar multiple
    
    Co-authored-by: Sam Morley <sam@inakleinbottle.com>

---
+++ tests/unit_tests/operators/test_operators_smul.cpp
@@ -0,0 +1,69 @@
+//
+// Created by sam on 28/01/2022.
+//
+
+#include <libalgebra/libalgebra.h>
+#include <libalgebra/operators/operators.h>
+
+#include <UnitTest++/UnitTest++.h>
+#include <libalgebra/alg_types.h>
+
+SUITE(test_operator_smul) {
+
+    struct fixture : alg_types<5, 5, Rational>
+    {
+        using multiplier_t = alg::operators::left_multiplication_operator<TENSOR>;
+
+        using smul_operator_t = alg::operators::scalar_multiply_operator<
+                TENSOR,
+                TENSOR,
+                multiplier_t,
+                S
+                >;
+        using operator_t = alg::operators::linear_operator<smul_operator_t, TENSOR, TENSOR>;
+        operator_t op;
+
+        fixture() : op(multiplier_t(TENSOR(LET(1), S(2))), S(3))
+        {}
+
+    };
+
+    TEST_FIXTURE(fixture, test_zero) {
+        TENSOR zero, expected;
+
+        CHECK_EQUAL(expected, op(zero));
+    }
+
+    TEST_FIXTURE(fixture, test_unit) {
+        TENSOR unit(S(1)), expected;
+
+        expected.add_scal_prod(typename TENSOR::KEY(LET(1)), S(6));
+
+        CHECK_EQUAL(expected, op(unit));
+    }
+
+    TEST_FIXTURE(fixture, test_letter_1) {
+        TENSOR arg(LET(1), S(1)), expected;
+
+        expected.add_scal_prod(typename TENSOR::KEY{LET(1), LET(1)}, S(6));
+
+        CHECK_EQUAL(expected, op(arg));
+    }
+
+    TEST_FIXTURE(fixture, test_letters_2345) {
+        TENSOR arg, expected;
+
+        arg.add_scal_prod(typename TENSOR::KEY(LET(2)), S(2));
+        arg.add_scal_prod(typename TENSOR::KEY(LET(3)), S(3));
+        arg.add_scal_prod(typename TENSOR::KEY(LET(4)), S(4));
+        arg.add_scal_prod(typename TENSOR::KEY(LET(5)), S(5));
+
+        expected.add_scal_prod(typename TENSOR::KEY {LET(1), LET(2)}, S(12));
+        expected.add_scal_prod(typename TENSOR::KEY {LET(1), LET(3)}, S(18));
+        expected.add_scal_prod(typename TENSOR::KEY {LET(1), LET(4)}, S(24));
+        expected.add_scal_prod(typename TENSOR::KEY {LET(1), LET(5)}, S(30));
+    }
+
+
+
+}
---
+++ tests/unit_tests/operators/test_tensor_defined_operator.cpp
@@ -0,0 +1,89 @@
+//
+// Created by sam on 27/01/2022.
+//
+
+#include <libalgebra/alg_types.h>
+#include <libalgebra/libalgebra.h>
+#include <libalgebra/operators/functionals.h>
+#include <libalgebra/operators/operators.h>
+#include <libalgebra/operators/tensor_operator.h>
+
+#include <UnitTest++/UnitTest++.h>
+
+SUITE(tensor_defined_operator)
+{
+
+    struct fixture : alg_types<5, 5, Rational> {
+        using types = alg_types<5, 5, Rational>;
+
+        using out_types = alg_types<5, 4, Rational>;
+        using OUT_TENSOR = typename out_types::TENSOR;
+        using TKEY = typename TENSOR::KEY;
+        using OKEY = typename OUT_TENSOR::KEY;
+
+        using functional_t = alg::operators::shuffle_tensor_functional<SHUFFLE_TENSOR, TENSOR>;
+        using op_impl = alg::operators::tensor_defined_operator<
+                functional_t,
+                OUT_TENSOR>;
+
+        using op_type = alg::operators::linear_operator<op_impl, TENSOR, OUT_TENSOR>;
+        op_type op;
+
+        fixture() : op{
+                {functional_t(SHUFFLE_TENSOR(S(1))), OUT_TENSOR(S(1))},
+                {functional_t(SHUFFLE_TENSOR(TKEY(LET(1)))), OUT_TENSOR(OKEY(LET(1)))},
+                {functional_t(SHUFFLE_TENSOR(TKEY(LET(2)))), OUT_TENSOR(OKEY(LET(2)))},
+                {functional_t(SHUFFLE_TENSOR(TKEY(LET(3)))), OUT_TENSOR(OKEY(LET(3)))},
+                {functional_t(SHUFFLE_TENSOR(TKEY(LET(4)))), OUT_TENSOR(OKEY(LET(4)))},
+                {functional_t(SHUFFLE_TENSOR(TKEY(LET(5)))), []() {
+                     OUT_TENSOR r;
+                     for (LET l = 1; l <= 4; ++l) {
+                         r += OUT_TENSOR(l, S(1));
+                     }
+                     return r;
+                 }()}}
+        {
+        }
+    };
+
+    TEST_FIXTURE(fixture, test_zero)
+    {
+        TENSOR in;
+        OUT_TENSOR expected;
+        auto out = op(in);
+
+        CHECK_EQUAL(expected, out);
+    }
+
+    TEST_FIXTURE(fixture, test_empty_word)
+    {
+        TENSOR in(S(1));
+        OUT_TENSOR expected(S(1));
+
+        auto out = op(in);
+        CHECK_EQUAL(expected, out);
+    }
+
+    TEST_FIXTURE(fixture, test_first_letters)
+    {
+        for (LET l = 1; l < ALPHABET_SIZE; ++l) {
+            TENSOR in(l, S(1));
+            OUT_TENSOR expected(l, S(1));
+
+            auto out = op(in);
+            CHECK_EQUAL(expected, out);
+        }
+    }
+
+    TEST_FIXTURE(fixture, test_final_letter)
+    {
+        TENSOR in(LET(ALPHABET_SIZE), S(1));
+        OUT_TENSOR expected;
+        for (LET l = 1; l < ALPHABET_SIZE; ++l) {
+            expected += OUT_TENSOR(l, S(1));
+        }
+        auto out = op(in);
+
+        CHECK_EQUAL(expected, out);
+    }
+}

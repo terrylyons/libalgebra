a3c22acfbfebe335bb3ad3445a6671f48a8a1fb7
Sam Morley <sam@inakleinbottle.com>
2022-02-23 14:34:55 +0000

    Fixed bad brackets

--- libalgebra/vectors/dense_vector.h
+++ libalgebra/vectors/dense_vector.h
@@ -107,7 +107,7 @@ public:
           m_dimension(m_data.size()),
           m_degree(0)
     {
-        if (m_data.size() != adjust_dimension(m_data.size()), degree_tag) {
+        if (m_data.size() != adjust_dimension(m_data.size(), degree_tag)) {
             resize_to_dimension(m_dimension);
         }
     }
@@ -128,7 +128,7 @@ public:
           m_dimension(0),
           m_degree(0)
     {
-        if (m_data.size() != adjust_dimension(m_data.size()), degree_tag) {
+        if (m_data.size() != adjust_dimension(m_data.size(), degree_tag)) {
             resize_to_dimension(m_dimension);
         }
     }
@@ -149,7 +149,7 @@ public:
           m_dimension(0),
           m_degree(0)
     {
-        if (m_data.size() != adjust_dimension(m_data.size()), degree_tag) {
+        if (m_data.size() != adjust_dimension(m_data.size(), degree_tag)) {
             resize_to_dimension(m_dimension);
         }
     }

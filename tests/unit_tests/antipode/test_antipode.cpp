ec84b1d6632a81297a3d77e08723a78dc14d2357
Sam Morley <sam@inakleinbottle.com>
2022-04-12 15:08:22 +0100

    Changed to generate a group-like element

--- tests/unit_tests/antipode/test_antipode.cpp
+++ tests/unit_tests/antipode/test_antipode.cpp
@@ -1,10 +1,9 @@
 #include <UnitTest++/UnitTest++.h>
 
-
 #include <libalgebra/libalgebra.h>
 
-#include <vector>
 #include <iostream>
+#include <vector>
 
 #include <libalgebra/alg_types.h>
 #include <libalgebra/multiplication_helpers.h>
@@ -18,9 +17,8 @@
 
 using alg::LET;
 
-template <typename Coeff, unsigned Width, unsigned Depth>
-struct SparseFixture
-{
+template<typename Coeff, unsigned Width, unsigned Depth>
+struct SparseFixture {
 
     typedef typename Coeff::S S;
     typedef typename Coeff::Q Q;
@@ -45,20 +43,22 @@ struct SparseFixture
         }
         return k;
     }
-
 };
 
-
-template <typename Coeff, unsigned Width, unsigned Depth>
-struct DenseFixture
-{
+template<typename Coeff, unsigned Width, unsigned Depth>
+struct DenseFixture {
 
     typedef typename Coeff::S S;
     typedef typename Coeff::Q Q;
 
+    static constexpr unsigned width = Width;
+    static constexpr unsigned depth = Depth;
+    typedef Coeff coeffs;
+
     typedef alg::free_tensor_basis<Width, Depth> TBASIS;
     typedef alg::vectors::dense_vector<TBASIS, Coeff> VECT;
     typedef alg::free_tensor<Coeff, Width, Depth, alg::vectors::dense_vector> TENSOR;
+    typedef alg::lie<Coeff, Width, Depth, alg::vectors::dense_vector> LIE;
 
     typedef typename TBASIS::KEY KEY;
 
@@ -76,7 +76,6 @@ struct DenseFixture
         }
         return k;
     }
-
 };
 
 struct RandomFixture : alg_types<5, 5, Rational> {
@@ -95,215 +94,212 @@ struct RandomFixture : alg_types<5, 5, Rational> {
     KEY kunit;
 
     RandomFixture() : reference_tensor(typename TENSOR::KEY(alg::LET(1))),
-                op(TENSOR(reference_tensor)), rng(std::random_device()()), rvg(-1, 1)
+                      op(TENSOR(reference_tensor)), rng(std::random_device()()), rvg(-1, 1)
     {}
 };
 
 SUITE(Antipode)
 {
-        typedef alg::coefficients::coefficient_field<float> float_field;
-        typedef DenseFixture<float_field, 4, 4> dense_fixture;
+    typedef alg::coefficients::coefficient_field<float> float_field;
+    typedef DenseFixture<float_field, 4, 4> dense_fixture;
 
-        // test: antipode(zero) == zero
+    // test: antipode(zero) == zero
 
-        TEST_FIXTURE(dense_fixture, DenseAntipodeZero)
-        {
-            // check: { } --> { }
+    TEST_FIXTURE(dense_fixture, DenseAntipodeZero)
+    {
+        // check: { } --> { }
 
-            TENSOR result = antipode(tzero);
+        TENSOR result = antipode(tzero);
 
-            CHECK_EQUAL(tzero, result);
-        }
+        CHECK_EQUAL(tzero, result);
+    }
 
-        // test: antipode(identity) == identity
+    // test: antipode(identity) == identity
 
-        TEST_FIXTURE(dense_fixture, DenseAntipodeIdentiy)
-        {
-            // check: { 1{} } --> { 1{} }
+    TEST_FIXTURE(dense_fixture, DenseAntipodeIdentiy)
+    {
+        // check: { 1{} } --> { 1{} }
 
-            TENSOR result = antipode(tunit);
+        TENSOR result = antipode(tunit);
 
-            CHECK_EQUAL(tunit, result);
-        }
+        CHECK_EQUAL(tunit, result);
+    }
 
-        // test: antipode(length 1 word) == - length 1 word
+    // test: antipode(length 1 word) == - length 1 word
 
-        TEST_FIXTURE(dense_fixture, DenseAntipodeOneLetter)
-        {
-            // check: { 1{1} } --> { -1{1} }
+    TEST_FIXTURE(dense_fixture, DenseAntipodeOneLetter)
+    {
+        // check: { 1{1} } --> { -1{1} }
 
-            LET k1[] = {1};
+        LET k1[] = {1};
 
-            TENSOR input_tensor(make_key(k1, 1));
+        TENSOR input_tensor(make_key(k1, 1));
 
-            TENSOR expected;
+        TENSOR expected;
 
-            expected.add_scal_prod(make_key(k1, 1), -1.0);
+        expected.add_scal_prod(make_key(k1, 1), -1.0);
 
-            TENSOR result = antipode(input_tensor);
+        TENSOR result = antipode(input_tensor);
 
-            CHECK_EQUAL(expected, result);
-        }
+        CHECK_EQUAL(expected, result);
+    }
 
-        // test: key/index look-ups for single even word tensor
+    // test: key/index look-ups for single even word tensor
 
-        TEST_FIXTURE(dense_fixture, DenseAntipodeOneEvenWord) {
+    TEST_FIXTURE(dense_fixture, DenseAntipodeOneEvenWord)
+    {
 
-            // check {1{12}} --> {1{21}}
+        // check {1{12}} --> {1{21}}
 
-            LET k12[] = {1, 2};
-            LET k21[] = {2, 1};
+        LET k12[] = {1, 2};
+        LET k21[] = {2, 1};
 
-            TENSOR input_tensor(make_key(k12, 2));
-            TENSOR expected(make_key(k21, 2));
+        TENSOR input_tensor(make_key(k12, 2));
+        TENSOR expected(make_key(k21, 2));
 
-            TENSOR result = antipode(input_tensor);
+        TENSOR result = antipode(input_tensor);
 
-            CHECK_EQUAL(expected, result);
+        CHECK_EQUAL(expected, result);
+    }
 
-        }
+    // test: key/index look-ups for single odd word tensor
 
-        // test: key/index look-ups for single odd word tensor
+    TEST_FIXTURE(dense_fixture, DenseAntipodeOneOddWord)
+    {
 
-        TEST_FIXTURE(dense_fixture, DenseAntipodeOneOddWord) {
+        // check {1{123}} --> {-1{321}}
 
-            // check {1{123}} --> {-1{321}}
+        LET k123[] = {1, 2, 3};
+        LET k321[] = {3, 2, 1};
 
-            LET k123[] = {1, 2, 3};
-            LET k321[] = {3, 2, 1};
+        TENSOR input_tensor(make_key(k123, 3));
 
-            TENSOR input_tensor(make_key(k123, 3));
+        TENSOR expected;
+        expected.add_scal_prod(make_key(k321, 3), -1.0);
 
-            TENSOR expected;
-            expected.add_scal_prod(make_key(k321, 3),-1.0);
+        TENSOR result = antipode(input_tensor);
 
-            TENSOR result = antipode(input_tensor);
+        CHECK_EQUAL(expected, result);
+    }
 
-            CHECK_EQUAL(expected, result);
+    // test: key/index look-ups for multiple word tensor
 
-        }
+    TEST_FIXTURE(dense_fixture, DenseAntipodeMultipleWord)
+    {
+        // check: { 1.0{11} 2.0{12} 3.0{21} 4.0{22} } --> { 1.0{11} 3.0{12} 2.0{21} 4.0{22} }
 
-        // test: key/index look-ups for multiple word tensor
+        LET k11[] = {1, 1};
+        LET k12[] = {1, 2};
+        LET k21[] = {2, 1};
+        LET k22[] = {2, 2};
 
-        TEST_FIXTURE(dense_fixture, DenseAntipodeMultipleWord)
-        {
-            // check: { 1.0{11} 2.0{12} 3.0{21} 4.0{22} } --> { 1.0{11} 3.0{12} 2.0{21} 4.0{22} }
+        LET k123[] = {1, 2, 3};
+        LET k321[] = {3, 2, 1};
 
-            LET k11[] = {1, 1};
-            LET k12[] = {1, 2};
-            LET k21[] = {2, 1};
-            LET k22[] = {2, 2};
+        TENSOR input_tensor;
 
-            LET k123[] = {1, 2, 3};
-            LET k321[] = {3, 2, 1};
+        input_tensor.add_scal_prod(make_key(k11, 2), 1.0);
+        input_tensor.add_scal_prod(make_key(k12, 2), 2.0);
+        input_tensor.add_scal_prod(make_key(k21, 2), 3.0);
+        input_tensor.add_scal_prod(make_key(k22, 2), 4.0);
 
-            TENSOR input_tensor;
+        input_tensor.add_scal_prod(make_key(k123, 3), 5.0);
 
-            input_tensor.add_scal_prod(make_key(k11, 2), 1.0);
-            input_tensor.add_scal_prod(make_key(k12, 2), 2.0);
-            input_tensor.add_scal_prod(make_key(k21, 2), 3.0);
-            input_tensor.add_scal_prod(make_key(k22, 2), 4.0);
+        TENSOR expected;
 
-            input_tensor.add_scal_prod(make_key(k123, 3), 5.0);
+        expected.add_scal_prod(make_key(k11, 2), 1.0);
+        expected.add_scal_prod(make_key(k12, 2), 3.0);
+        expected.add_scal_prod(make_key(k21, 2), 2.0);
+        expected.add_scal_prod(make_key(k22, 2), 4.0);
 
-            TENSOR expected;
+        expected.add_scal_prod(make_key(k321, 3), -5.0);
 
-            expected.add_scal_prod(make_key(k11, 2), 1.0);
-            expected.add_scal_prod(make_key(k12, 2), 3.0);
-            expected.add_scal_prod(make_key(k21, 2), 2.0);
-            expected.add_scal_prod(make_key(k22, 2), 4.0);
+        TENSOR result = antipode(input_tensor);
 
-            expected.add_scal_prod(make_key(k321, 3), -5.0);
+        std::cout << "input_tensor=" << input_tensor << std::endl;
 
-            TENSOR result = antipode(input_tensor);
+        std::cout << "expected=" << expected << std::endl;
 
-            std::cout << "input_tensor=" << input_tensor << std::endl;
+        std::cout << "result=" << result << std::endl;
 
-            std::cout << "expected=" << expected << std::endl;
+        CHECK_EQUAL(expected, result);
+    }
 
-            std::cout << "result=" << result << std::endl;
+    // tests using random tensors: antipode(a) * a == Identity
 
-            CHECK_EQUAL(expected, result);
-        }
+    TEST_FIXTURE(RandomFixture, DenseAntipodeGroupLike)
+    {
+        LIE incr;
+        incr.add_scal_prod(typename LIE::KEY(1), typename LIE::SCALAR(1));
+        incr.add_scal_prod(typename LIE::KEY(2), typename LIE::SCALAR(2));
+        incr.add_scal_prod(typename LIE::KEY(3), typename LIE::SCALAR(3));
+        incr.add_scal_prod(typename LIE::KEY(4), typename LIE::SCALAR(4));
 
+        alg::maps<dense_fixtureDenseAntipodeIdentiyHelper::coeffs, dense_fixtureDenseAntipodeIdentiyHelper::width, dense_fixtureDenseAntipodeIdentiyHelper::depth, TENSOR, LIE> maps;
 
-        // tests using random tensors: antipode(a) * a == Identity
+        auto input_tensor = exp(maps.l2t(incr));
 
-        TEST_FIXTURE(RandomFixture, DenseAntipodeGroupLike)
-        {
+        auto result = antipode(input_tensor) * input_tensor;
 
-            auto input_tensor = rvg(rng);
+        double tolerance = 0.01;// TODO: change this!
 
-            auto result = antipode(input_tensor) * input_tensor;
-
-            double tolerance = 0.01; // TODO: change this!
-
-            for (auto cit = result.begin(); cit != result.end(); ++cit)
-            {
-                if (cit->key() == kunit)
-                {
-                    // check 1
-                    CHECK_CLOSE(1.0, cit->value(), tolerance);
-                }
-                else
-                {
-                    // check 0
-                    CHECK_CLOSE(0.0, cit->value(), tolerance);
-                }
+        for (auto cit = result.begin(); cit != result.end(); ++cit) {
+            if (cit->key() == kunit) {
+                // check 1
+                CHECK_CLOSE(1.0, cit->value(), tolerance);
             }
-
+            else {
+                // check 0
+                CHECK_CLOSE(0.0, cit->value(), tolerance);
+            }
         }
+    }
 
-        typedef SparseFixture<float_field, 4, 4> sparse_fixture;
+    typedef SparseFixture<float_field, 4, 4> sparse_fixture;
 
-        // test: key/index look-ups for multiple word tensor
+    // test: key/index look-ups for multiple word tensor
 
-        TEST_FIXTURE(sparse_fixture, SparseAntipodeMultipleWord)
-        {
-            // check: { 1.0{11} 2.0{12} 3.0{21} 4.0{22} 5.0{123} } --> { 1.0{11} 3.0{12} 2.0{21} 4.0{22} -5.0{321} }
+    TEST_FIXTURE(sparse_fixture, SparseAntipodeMultipleWord)
+    {
+        // check: { 1.0{11} 2.0{12} 3.0{21} 4.0{22} 5.0{123} } --> { 1.0{11} 3.0{12} 2.0{21} 4.0{22} -5.0{321} }
 
-            LET k11[] = {1, 1};
-            LET k12[] = {1, 2};
-            LET k21[] = {2, 1};
-            LET k22[] = {2, 2};
+        LET k11[] = {1, 1};
+        LET k12[] = {1, 2};
+        LET k21[] = {2, 1};
+        LET k22[] = {2, 2};
 
-            LET k123[] = {1, 2, 3};
-            LET k321[] = {3, 2, 1};
+        LET k123[] = {1, 2, 3};
+        LET k321[] = {3, 2, 1};
 
-            TENSOR input_tensor;
+        TENSOR input_tensor;
 
-            input_tensor.add_scal_prod(make_key(k11, 2), 1.0);
-            input_tensor.add_scal_prod(make_key(k12, 2), 2.0);
-            input_tensor.add_scal_prod(make_key(k21, 2), 3.0);
-            input_tensor.add_scal_prod(make_key(k22, 2), 4.0);
+        input_tensor.add_scal_prod(make_key(k11, 2), 1.0);
+        input_tensor.add_scal_prod(make_key(k12, 2), 2.0);
+        input_tensor.add_scal_prod(make_key(k21, 2), 3.0);
+        input_tensor.add_scal_prod(make_key(k22, 2), 4.0);
 
-            input_tensor.add_scal_prod(make_key(k123, 3), 5.0);
+        input_tensor.add_scal_prod(make_key(k123, 3), 5.0);
 
+        TENSOR expected;
 
+        expected.add_scal_prod(make_key(k11, 2), 1.0);
+        expected.add_scal_prod(make_key(k12, 2), 3.0);
+        expected.add_scal_prod(make_key(k21, 2), 2.0);
+        expected.add_scal_prod(make_key(k22, 2), 4.0);
 
-            TENSOR expected;
+        expected.add_scal_prod(make_key(k321, 3), -5.0);
 
-            expected.add_scal_prod(make_key(k11, 2), 1.0);
-            expected.add_scal_prod(make_key(k12, 2), 3.0);
-            expected.add_scal_prod(make_key(k21, 2), 2.0);
-            expected.add_scal_prod(make_key(k22, 2), 4.0);
+        TENSOR result;
 
-            expected.add_scal_prod(make_key(k321, 3), -5.0);
+        result = antipode(input_tensor);
 
+        //            std::cout << "input_tensor=" << input_tensor << std::endl;
+        //
+        //            std::cout << "expected=" << expected << std::endl;
+        //
+        //            std::cout << "result=" << result << std::endl;
 
-
-            TENSOR result;
-
-            result = antipode(input_tensor);
-
-//            std::cout << "input_tensor=" << input_tensor << std::endl;
-//
-//            std::cout << "expected=" << expected << std::endl;
-//
-//            std::cout << "result=" << result << std::endl;
-
-            CHECK_EQUAL(expected, result);
-        }
-
+        CHECK_EQUAL(expected, result);
+    }
 
 }// SUITE antipode

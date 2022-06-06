b1cb76dd54026ad2182889e2263fd24555721a3e
terrylyons <tlyons@maths.ox.ac.uk>
2022-06-06 13:51:58 +0100

    Corrected the messed up characters in Greg Gyurko's name. (#68)
    * Corrected the messed up characters in Greg Gyurko's name.
    
    * Replaced Greg's name with the correct accent this time Gyurkó
    
    Co-authored-by: terrylyons <terrylyons@github.com>
    Co-authored-by: inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>

--- libalgebra/vectors/sparse_vector.h
+++ libalgebra/vectors/sparse_vector.h
@@ -1,6 +1,6 @@
 ﻿/* *************************************************************
 Copyright 2010 Terry Lyons, Stephen Buckley, Djalil Chafai,
-Greg Gyurk� and Arend Janssen.
+Greg Gyurkó and Arend Janssen.
 
 Distributed under the terms of the GNU General Public License,
 Version 3. (See accompanying file License.txt)

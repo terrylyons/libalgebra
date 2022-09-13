e5d85d976c7c8da3c5c8690d7ebc74f1db8f75ef
Philippa Rubin <philippa.rubin@stfc.ac.uk>
2022-09-13 09:37:02 +0100

    Add tensor multiplcation benchmarks to cmake

---
+++ benchmarks/multiplication_bm.cpp
@@ -0,0 +1,22 @@
+#include <benchmark/benchmark.h>
+#include <iostream>
+
+template<int W, int D>
+static void BM_multiplication(benchmark::State& state)
+{
+    std::cout << "Hello, Benchmarks!" << std::endl;
+
+    for (auto _ : state) {
+////        auto result = tiled_inverse(t);
+//        benchmark::DoNotOptimize(result);
+        benchmark::ClobberMemory();
+    }
+//
+//    state.SetBytesProcessed(state.iterations()*2 * sizeof(double) * playground::tensor_alg_size(W, D));
+//    state.counters["block_letters"] = tiled_inverse.block_letters;
+//    state.counters["block_size"] = tiled_inverse.block_size * sizeof(double);
+//    state.counters["blob size"] = sizeof(double) * playground::tensor_alg_size(W, D);
+
+}
+
+BENCHMARK_TEMPLATE(BM_multiplication, 1, 1);
